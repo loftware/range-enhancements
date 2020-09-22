0672af2601bb75ccdc749ac1ccd29b23e29ae032
Sam Lazarus <tyu.sam@gmail.com>
2020-09-21 23:48:55 -0400

    Removed note about maybe naming

--- Sources/RangeEnhancements/InclusiveRange.swift
+++ Sources/RangeEnhancements/InclusiveRange.swift
@@ -1,9 +1,4 @@
 /// A range which contains all of its boundaries.
-// Note: The boundaries in this protocol are prefixed with maybe as otherwise
-// it is impossible to conform to this protocol for anything that already
-// publishes a non-optional `lowerBound`. The names conflict, preventing you
-// from providing conformance, and optional computed properties do not satisfy
-//
 public protocol InclusiveRange: RangeProtocol {
     /// The range's lower bound if it has one.
     var lowerBound: Bound? { get }
